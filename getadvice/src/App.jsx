337d4ce69b2f9d662dccb93ef8617852c6616261
arif jamal <arif.jamal@squintmetrics.com>
2024-04-04 11:33:16 +0530

    get advice

---
+++ getadvice/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css';
+
+function App() {
+  const [advice, setAdvice] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function getAdvice() {
+    setLoading(true);
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setLoading(false);
+    setAdvice(data.slip.advice);
+    setCount(c => c + 1);
+  }
+
+  return (
+    <div className="advice-area">
+      <div className="advice-inner">
+        {loading ?
+          <div class="loader"></div> :
+          <h1>{advice ? advice : "Please click the get advice button.Your advice gonna surprise you!"}</h1>
+        }
+        <button onClick={getAdvice}>Get Advice</button>
+        </div>
+    </div>
+  )
+}
+
+export default App;
